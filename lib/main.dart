0e93e2e9b0b36d424658e11fe9a9d79293aca67e
YA SYN <122826162+elidrissitv@users.noreply.github.com>
2025-04-08 16:20:41 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
-import 'pages/profile_screen.dart';
-import 'pages/discovery_screen.dart';
-import 'pages/comparateur_screen.dart';
-import 'widgets/bottom_navigation.dart';
+import 'widgets/main_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,70 +21,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Roboto',
       ),
-      onGenerateRoute: (settings) {
-        if (settings.name == '/') {
-          return MaterialPageRoute(
-            builder: (context) => MainNavigation(
-              initialIndex: settings.arguments as int? ?? 0,
-            ),
-          );
-        }
-        return null;
-      },
-      home: const MainNavigation(),
-    );
-  }
-}
-
-class MainNavigation extends StatefulWidget {
-  final int initialIndex;
-
-  const MainNavigation({
-    super.key,
-    this.initialIndex = 0,
-  });
-
-  @override
-  State<MainNavigation> createState() => MainNavigationState();
-}
-
-class MainNavigationState extends State<MainNavigation> {
-  late int _currentIndex;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentIndex = widget.initialIndex;
-  }
-
-  final List<Widget> _pages = [
-    const HomePage(),
-    DiscoveryScreen(),
-    const Center(child: Text('Réservation')),
-    const ComparateurScreen(),
-    const Center(child: Text('Communaute')),
-    ProfileScreen(),
-  ];
-
-  void onNavigationTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Social Safari'),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigation(
-        currentIndex: _currentIndex,
-        onTap: onNavigationTap,
-      ),
+      home: const MainLayout(),
     );
   }
 }
